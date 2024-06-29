783bcd60c5ae4cfc360f5b24b84b2f6462fd224e
Nolan-James-Dev <nj@nolanjames.me>
2024-06-29 20:37:05 +0100

    initial commit

---
+++ finance-manager-api/src/main/java/me/nolanjames/financemanagerapi/FinanceManagerApiApplication.java
@@ -0,0 +1,13 @@
+package me.nolanjames.financemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagerApiApplication.class, args);
+    }
+
+}
